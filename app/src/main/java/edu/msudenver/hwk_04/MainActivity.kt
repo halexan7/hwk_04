a3c1d67d6c9731f8bcf208a770ae87f5247b2a8b
halexan7 <halexan7@msudenver.edu>
2022-06-22 21:23:23 -0600

    new

--- app/src/main/java/edu/msudenver/hwk_04/MainActivity.kt
+++ app/src/main/java/edu/msudenver/hwk_04/MainActivity.kt
@@ -2,10 +2,15 @@ package edu.msudenver.hwk_04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText: EditText = findViewById(R.id.edtText)
+        val shareButton: Button = findViewById(R.id.shareButton)
     }
 }
